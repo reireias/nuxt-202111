484c181dafe6d9c989175a9809f75fb1533acd64
reireias <reireias@gmail.com>
2021-11-18 11:03:52 +0900

    Add pages

---
+++ pages/foo/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>/foo</h1>
+    <nuxt-link to="/foo">/foo</nuxt-link>
+    <nuxt-link to="/foo/">/foo/</nuxt-link>
+    <nuxt-link to="/foo.html">/foo.html</nuxt-link>
+    <nuxt-link to="/foo/index.html">/foo/index.html</nuxt-link>
+    <br>
+    <nuxt-link to="/foo/sub">/foo/sub</nuxt-link>
+    <nuxt-link to="/foo/sub/">/foo/sub/</nuxt-link>
+    <nuxt-link to="/foo/sub.html">/foo/sub.html</nuxt-link>
+  </div>
+</template>
